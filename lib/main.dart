dc95cdc250f699d55563768b86203356f3a99623
Amitabh <mouryamit450@gmail.com>
2022-09-15 23:04:13 +0530

    my new flutter app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My app',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body:  Container(
+          child:Center(
+            child: Text('hello amit mourya'),
+            ),
+        ),
+      ),
+    );
+  }
+}
